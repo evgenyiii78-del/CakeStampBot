83ce2d7531f9ab468a6c94c993283fba36272c13
evgenyiii78-del <evgenyiii78@gmail.com>
2026-08-29 18:30:12 +0300

    v1.7.2 route text stamps through size and preview wrapper

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,2 +1,3 @@
-from .stamp_engine import build_stamp_from_text, build_stamp_from_image
+from .stamp_v172 import build_stamp_from_text
+from .stamp_engine import build_stamp_from_image
 from .topper_engine import build_topper_from_text
